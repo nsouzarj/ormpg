62869f3359fd62cc9fcf1ecb6a31d5eeb79bc592
nsouzarj <nsouzarj@outlook.com>
2021-08-02 14:32:17 -0300

    Feito

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,20 @@ import { createConnection } from "typeorm";
 import { Uf } from "./entity/Uf";
 
 createConnection().then(connection => {
+
     console.log("Startando o serviço de back-end...")
 
     let app = express();
+
     app.use(express.json())
+    
     app.get("/estados", async function (req: Request, res: Response) {
         let uf = await connection.manager.createQueryBuilder(Uf, "uf").where("uf.iduf >= 1").orderBy("uf.sigla", "ASC").cache(true).getMany();
         res.json(uf);
     });
+
+
+    
 
     app.get("/estados/:iduf", async function (req: Request, res: Response) {
         let uf = await connection.manager.findOne(Uf, req.params.iduf)
