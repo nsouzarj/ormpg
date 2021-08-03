db1d47498dd2dec76122fb3627a7495eb4843087
nsouzarj <nsouzarj@outlook.com>
2021-08-02 23:35:24 -0300

    Teste de ajuste

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-import "reflect-metadata";
-import * as express from "express";
-import { Request, Response } from "express";
-import { createConnection } from "typeorm";
-import { Uf } from "./entity/Uf";
+import "reflect-metadata"
+import * as express from "express"
+import { Request, Response } from "express"
+import { createConnection } from "typeorm"
+import { Uf } from "./entity/Uf"
 
 createConnection().then(connection => {
 
     console.log("Startando o serviço de back-end...")
 
-    let app = express();
-
+    let app = express()
     app.use(express.json())
-    
     app.get("/estados", async function (req: Request, res: Response) {
         let uf = await connection.manager.createQueryBuilder(Uf, "uf").where("uf.iduf >= 1").orderBy("uf.sigla", "ASC").cache(true).getMany();
-        res.json(uf);
-    });
+        res.json(uf)
+    })
 
-
-    
 
     app.get("/estados/:iduf", async function (req: Request, res: Response) {
         let uf = await connection.manager.findOne(Uf, req.params.iduf)
-        return res.json(uf);
-    });
+        return res.json(uf)
+    })
 
-    app.listen(3000);
+    app.listen(3000)
 
-}).catch(error => console.log(error));
+}).catch(error => console.log(error))
